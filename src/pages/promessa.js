71418f3e0e1b72250fea211bc1598f943c20d882
evelynzagolino <120220285+evelynzagolino@users.noreply.github.com>
2024-10-28 14:20:14 +0000

    aula 28/10

--- src/pages/promessa.js
+++ src/pages/promessa.js
@@ -1,19 +1,28 @@
 import { Container } from "react-bootstrap";
 import Footer from "./modulos/footer";
 import Menu from "./modulos/navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function Promessa(){
-const [pao, setpao] = useState("")
-    setTimeout(() => {
-        document.title = "Promessa é Dívida";
-        setpao("é Dívida");
-    }, 4000);
+export default function Promessa() {
+    const [pao, setpao] = useState("")
+    useEffect(() => {
+        setTimeout(async() => {
+            document.title = "Promessa é Dívida";
+            const espera= await mudaPao("é Dívida")
+            setpao(espera);
+        }, 4000);
+    });
     return <>
-    <Menu />
-    <Container>
-        Promessa {pao==="" ? :pao}
-    </Container>
-    <Footer />
-</>
+        <Menu />
+        <Container>
+            Promessa {pao === "" ? <img src="./loading.gif" width={50} height={40} /> : pao}
+        </Container>
+        <Footer />
+    </>
+}
+function mudaPao(texto){
+    return new Promise((resolva)=>
+        setTimeout(()=>{
+            resolva(texto)
+        },1000));
 }
